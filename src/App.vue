b3c3f102d9a10d135da9697e660e392afd32a4c9
Barron <bfd27@cornell.edu>
2020-06-21 19:30:14 -0400

    changed number items per page from 5 to 20

--- src/App.vue
+++ src/App.vue
@@ -101,7 +101,7 @@
     <v-data-table
       :headers="headers"
       :items="condors"
-      :items-per-page="5"
+      :items-per-page="20"
       class="elevation-1"
     ></v-data-table>
   </v-app>
