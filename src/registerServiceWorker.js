4b9e9253423f50fc6e40c725f1878c8601b8de7a
Barron <bfd27@cornell.edu>
2020-06-24 15:23:18 -0400

    fixed cache?

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -26,6 +26,14 @@ if (process.env.NODE_ENV === 'production') {
     },
     updated () {
       console.log('New content is available; please refresh.')
+      caches.keys().then(cacheNames => {
+        cacheNames.forEach(cacheName => {
+          caches.delete(cacheName);
+        });
+      });
+      setTimeout(() => {
+        window.location.reload(true)
+      }, 1000)
     },
     offline () {
       console.log('No internet connection found. App is running in offline mode.')
